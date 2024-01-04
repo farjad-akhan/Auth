8a477f0dce5611b96a82a89d460c25429c85c8ed
farjad-akhan <farjad.ajfa.khan0@gmail.com>
2024-01-05 01:54:51 +0530

    Added signin with jwt creation

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,5 +1,7 @@
+
 const { StatusCodes } = require('http-status-codes');
 const { UserRepository } = require('../repositories');
+const {Auth} = require('../utils/common');
 const AppError = require('../utils/errors/app-error');
 const userRepo = new UserRepository();
 
@@ -19,6 +21,27 @@ async function create(data) {
     }
 }
 
+async function signin(data){
+    try {
+        const user = await userRepo.getUserByEmail(data.email);
+        if(!user){
+            throw new AppError('No user found for the given email', StatusCodes.NOT_FOUND);
+        }
+        const passwordMatch = Auth.checkPassword(data.password , user.password);
+        if(!passwordMatch){
+            throw new AppError('Invalid password', StatusCodes.BAD_REQUEST);
+        }
+        const jwt = Auth.createToken({id:user.id, email:user.email});
+        return jwt;
+    } catch (error) {
+        if(error instanceof AppError) throw error;
+        console.log(error);
+        throw new AppError('Something went wrong', StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+}
+
+
 module.exports = {
-    create
+    create,
+    signin
 }
